6d3d47ee1a17e3b6eacacfcdd16c5f4e774ffd05
ANWDM <pv4003337@gmail.com>
2023-05-21 11:43:21 +0530

    some changes

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,23 +1,39 @@
-import profile from '../assets/profile.png'
-import video from '../assets/video.png'
-import call from '../assets/call.png'
-import dot from '../assets/dot.jpg'
-
+import profile from '../assets/profile.png';
+import video from '../assets/video.png';
+import call from '../assets/call.png';
+import dot from '../assets/dot.jpg';
 
 const Chat = () => {
-    return (
-        <div className="mt-10 ml-2 flex justify-between w-[70%]">
-            <div className='flex'>
-            <img src={profile} className='w-12 h-10' alt="" />
-            <h1 className='ml-1 mt-1'>Zilan</h1>
-            </div>
-            <div className='flex gap-4'>
-                <img src={video} className='h-8' alt="" />
-                <img src={call} className='h-8' alt="" />
-                <img src={dot} className='h-8' alt="" />
-            </div>
-        </div> 
-    )
-}
+  return (
+    <div className="mt-10 ml-2 flex flex-col items-stretch h-28 w-2/3 bg-gray-100 border-t border-gray-300 p-2">
+      <div className="flex justify-between items-center h-full">
+        <div className="flex items-center">
+          <img
+            src={profile}
+            className="w-12 h-12 rounded-full object-cover"
+            alt=""
+          />
+          <h1 className="ml-2 text-xl font-bold">Zilan</h1>
+        </div>
 
-export default Chat
+        <div className="flex gap-4">
+          <img src={video} className="h-6" alt="" />
+          <img src={call} className="h-6" alt="" />
+          <img src={dot} className="h-6" alt="" />
+        </div>
+      </div>
+
+      <div className="mt-4 border border-gray-300 px-4 py-2 rounded-full w-full bg-white">
+        <button>😊</button>
+        <input
+          type="text"
+          placeholder="Type a message"
+          className="border border-gray-300 px-4 py-2 rounded-full w-full bg-white"
+          
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
